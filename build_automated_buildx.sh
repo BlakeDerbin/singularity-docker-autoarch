01492aad128d93a924021b53bb49411bb3451e50
Blake Derbin <blake.derbin@hotmail.com>
2021-09-15 10:53:04 +1000

    Added options: push to dockerhub account, multiple architecture builds

--- build_automated_buildx.sh
+++ build_automated_buildx.sh
@@ -10,70 +10,118 @@ DOCKERFILE_DIR=/singularity-docker-autoarch
 
 print_usage() {
   printf " Usage:
-        -u: user directory path i.e. /home/ubuntu  
-        -s: Singularity version i.e. 3.8.3  
-        -d: Docker buildx version i.e. 0.6.3
-        -g: GO version i.e. 1.16.4
+        -u: user directory path 
+            -u /home/ubuntu  
+        -s: Singularity version 
+            -s 3.8.3  
+        -d: Docker buildx version 
+            -d 0.6.3
+        -g: GO version 
+            -g 1.16.4
+        -o: Additional Architectures you wish to build in, must follow this format: 
+            -o linux/arm64,linux/arm/v7 
+        -a Dockerhub account, for pushing images to dockerhub
+            -a dockerhubAccount1
 
 "
 }
 
 # Optional arguments for the script
-while getopts 'u:s:d:g:' flag; do
+while getopts 'u:s:d:g:a:o:' flag; do
   case "${flag}" in
     u) USER_DIR=$OPTARG ;;
     s) SINGULARITY_VERSION=$OPTARG ;;
     d) DOCKER_BUILDX_VERSION=$OPTARG ;;
     g) GO_VERSION=$OPTARG ;;
+    o) ADDITIONAL_ARCH=$OPTARG ;;
+    a) DOCKERHUB_ACCOUNT=$OPTARG ;;
     *) print_usage
        exit 1 ;;
   esac
 done
 
-echo "User directory set to ${USER_DIR}"
+host_checks() {
+    printf "User directory set to ${USER_DIR}\n"
 
-# Checks the cpu architecture
-if [ $(uname -p) = "aarch64" ] || [ $(uname -p) = "arm64" ]
+    # Checks the cpu architecture
+    if [ $(uname -p) = "aarch64" ] || [ $(uname -p) = "arm64" ]
+    then
+        ARCH="arm64"
+        PLATFORM="${OS}/${ARCH},${ADDITIONAL_ARCH}"
+    else
+        ARCH="amd64"
+        PLATFORM="${OS}/${ARCH},${ADDITIONAL_ARCH}"
+    fi
+
+    # Checks if the local user directory container the .docker directory
+    if [ ! -d ${USER_DIR}/.docker ]
+    then
+        mkdir ${USER_DIR}/.docker
+        cd ${USER_DIR}/.docker
+        mkdir cli-plugins
+    else
+        cd ${USER_DIR}/.docker
+        if [ ! -d cli-plugins ] ; then mkdir cli-plugins ; fi
+    fi
+
+    DOCKER_BUILDX_FILE=buildx-v${DOCKER_BUILDX_VERSION}.${OS}-${ARCH}
+    cd ${USER_DIR}/.docker/cli-plugins
+
+    # Checks if the docker buildx file doesn't exist
+    if [ ! -f ${DOCKER_BUILDX_FILE}* ]
+    then
+        wget "https://github.com/docker/buildx/releases/download/v${DOCKER_BUILDX_VERSION}/${DOCKER_BUILDX_FILE}"
+        chmod a+x ${USER_DIR}/.docker/cli-plugins/${DOCKER_BUILDX_FILE}
+        echo "Docker Buildx version: ${DOCKER_BUILDX_VERSION} downloaded"
+    fi
+
+    build_containers
+}
+
+build_containers() {
+    printf "\nCreating docker container: singularity:v${SINGULARITY_VERSION}, using Docker Buildx on CPU architecture: ${PLATFORM}\n"
+
+    if [ -z ${DOCKERHUB_ACCOUNT} ]
+    then
+        printf "No Dockerhub account provided, building locally\n"
+        BUILD_NAME=singularity:${SINGULARITY_VERSION}
+    else
+        printf "Login to Dockerhub ID: ${DOCKERHUB_ACCOUNT}, "
+        docker login -u ${DOCKERHUB_ACCOUNT}
+        BUILD_NAME=${DOCKERHUB_ACCOUNT}/singularity:${SINGULARITY_VERSION}
+        STORE_METHOD="--push"
+        printf "Container builds will be pushed Dockerhub account ${DOCKERHUB_ACCOUNT}\n"
+    fi
+
+    # Creating the docker container for singularity using buildx
+    cd ${USER_DIR}/${DOCKERFILE_DIR}
+    docker buildx create \
+        --driver-opt env.http_proxy=$http_proxy \
+        --driver-opt env.https_proxy=$https_proxy \
+        --driver-opt '"env.no_proxy='$no_proxy'"' \
+        --driver-opt network=host \
+        --platform ${PLATFORM} \
+        --name singularity-builder
+
+    docker buildx use singularity-builder
+
+    docker buildx build \
+        --build-arg http_proxy=$http_proxy \
+        --build-arg https_proxy=$https_proxy \
+        --build-arg SINGULARITY_VERSION=${SINGULARITY_VERSION} \
+        --build-arg GO_VERSION=${GO_VERSION} \
+        -t ${BUILD_NAME} \
+        --platform ${PLATFORM} \
+        . ${STORE_METHOD}
+    
+    docker rm buildx_buildkit_singularity-builder0 -f
+    docker logout
+}
+
+if [ -d ${USER_DIR} ]
 then
-    ARCH="arm64"
-    PLATFORM="${OS}/${ARCH}"
+    host_checks
 else
-    ARCH="amd64"
-    PLATFORM="${OS}/${ARCH}"
+    printf "${USER_DIR} is not a valid directory, ensure that the correct home directory is set\n" 
+    exit 1;
 fi
-
-# Checks if the local user directory container the .docker directory
-if [ ! -d ${USER_DIR}/.docker ]
-then
-    mkdir ${USER_DIR}/.docker
-    cd ${USER_DIR}/.docker
-    mkdir cli-plugins
-else
-    cd ${USER_DIR}/.docker
-    if [ ! -d cli-plugins ] ; then mkdir cli-plugins ; fi
-fi
-
-DOCKER_BUILDX_FILE=buildx-v${DOCKER_BUILDX_VERSION}.${OS}-${ARCH}
-cd ${USER_DIR}/.docker/cli-plugins
-
-# Checks if the docker buildx file doesn't exist
-if [ ! -f ${DOCKER_BUILDX_FILE}* ]
-then
-    wget "https://github.com/docker/buildx/releases/download/v${DOCKER_BUILDX_VERSION}/${DOCKER_BUILDX_FILE}"
-    chmod a+x ${USER_DIR}/.docker/cli-plugins/${DOCKER_BUILDX_FILE}
-    echo "Docker Buildx version: ${DOCKER_BUILDX_VERSION} downloaded"
-fi
-
-echo "\nCreating docker container: singularity:v${SINGULARITY_VERSION}, using Docker Buildx on CPU architecture: ${PLATFORM} \n"
-
-# Creating the docker container for singularity using buildx
-cd ${USER_DIR}/${DOCKERFILE_DIR}
-docker buildx create --driver-opt env.http_proxy=$http_proxy --driver-opt env.https_proxy=$https_proxy --driver-opt '"env.no_proxy='$no_proxy'"' --driver-opt network=host --name singularity-builder
-docker buildx use singularity-builder
-docker buildx build \
-    --build-arg http_proxy=$http_proxy \
-    --build-arg https_proxy=$https_proxy \
-    --build-arg SINGULARITY_VERSION=${SINGULARITY_VERSION} \
-    --build-arg GO_VERSION=${GO_VERSION} \
-    -t singularity:${SINGULARITY_VERSION} \
-    --platform ${PLATFORM} . --load
